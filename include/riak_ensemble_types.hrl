823954d08c38699bb8bcd04e7e7d7821d63356f7
Joseph Blomstedt <joe@basho.com>
2013-10-21 02:58:32 -0700

    Work in progress

---
+++ include/riak_ensemble_types.hrl
@@ -0,0 +1,25 @@
+-type ensemble_id() :: term().
+-type peer_id() :: {term(), node()}.
+-type leader_id() :: undefined | peer_id().
+-type fixme() :: any().
+-type views() :: [[peer_id()]].
+-type peer_change() :: term(). %% FIXME
+-type change_error() :: already_member | not_member.
+-type std_reply() :: timeout | failed | unavailable | nack | {ok, term()}.
+-type maybe_pid() :: pid() | undefined.
+-type peer_pids() :: [{peer_id(), maybe_pid()}].
+-type peer_reply() :: {peer_id(), term()}.
+-type epoch() :: non_neg_integer().
+-type seq() :: non_neg_integer().
+
+-record(ensemble_info, {mod     = riak_ensemble_basic_backend :: module(),
+                        args    = []                          :: [any()],
+                        leader                                :: leader_id(),
+                        members                               :: [peer_id()],
+                        seq                                   :: {integer(), integer()}
+                       }).
+-type ensemble_info() :: #ensemble_info{}.
+
+%% -type ensemble_info() :: {leader_id(), [peer_id()], {integer(), integer()}, module(), [any()]}.
+
+-define(ENSEMBLE_TICK, 200).
---
+++ src/riak_ensemble_backend.erl
@@ -0,0 +1,112 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2013 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc
+%% This module defines riak_ensemble_backend behavior used to implement
+%% custom peer behavior.
+
+-module(riak_ensemble_backend).
+
+-include_lib("riak_ensemble_types.hrl").
+
+%% API
+-export([start/4,
+         get_obj/3,
+         set_obj/4,
+         reply/2]).
+
+%%===================================================================
+
+-type state() :: any().
+-type obj()   :: any().
+-type key()   :: any().
+-type value() :: any().
+-type from()  :: {{_,_}, peer_id()} |
+                 {riak_ensemble_msg:msg_from(), peer_id()}.
+
+-export_type([from/0]).
+
+%%===================================================================
+
+%% Initialization callback that returns initial module state.
+-callback init(ensemble_id(), peer_id(), [any()]) -> state().
+
+%% Create a new opaque key/value object using whatever representation
+%% the defining module desires.
+-callback new_obj(epoch(), seq(), key(), value()) -> obj().
+
+%% Accessors to retrieve epoch/seq/key/value from an opaque object.
+-callback obj_epoch(obj()) -> epoch().
+-callback obj_seq  (obj()) -> seq().
+-callback obj_key  (obj()) -> term().
+-callback obj_value(obj()) -> term().
+
+%% Setters for epoch/seq/value for opaque objects.
+-callback set_obj_epoch(epoch(), obj()) -> obj().
+-callback set_obj_seq  (seq(),   obj()) -> obj().
+-callback set_obj_value(term(),  obj()) -> obj().
+
+%% Callback for get operations. Responsible for sending a reply to the
+%% waiting `from' process using {@link reply/2}.
+-callback get(key(), from(), state()) -> state().
+
+%% Callback for put operations. Responsible for sending a reply to
+%% the waiting `from' process using {@link reply/2}.
+-callback put(key(), obj(), from(), state()) -> state().
+
+%% Callback for periodic leader tick. This function is called periodically
+%% by an elected leader. Can be used to implement custom housekeeping.
+-callback tick(epoch(), seq(), peer_id(), views(), state()) -> state().
+
+%%===================================================================
+
+start(Mod, Ensemble, Id, Args) ->
+    Mod:init(Ensemble, Id, Args).
+
+get_obj(Mod, X, Obj) ->
+    case X of
+        epoch ->
+            Mod:obj_epoch(Obj);
+        seq ->
+            Mod:obj_seq(Obj);
+        key ->
+            Mod:obj_key(Obj);
+        value ->
+            Mod:obj_value(Obj)
+    end.
+
+set_obj(Mod, X, Val, Obj) ->
+    case X of
+        epoch ->
+            Mod:set_obj_epoch(Val, Obj);
+        seq ->
+            Mod:set_obj_seq(Val, Obj);
+        value ->
+            Mod:set_obj_value(Val, Obj)
+    end.
+
+-spec reply(from(), any()) -> ok.
+reply({{To, Tag}, _Id}, Reply) ->
+    catch To ! {Tag, Reply},
+    ok;
+reply({From, Id}, Reply) ->
+    riak_ensemble_msg:reply(From, Id, Reply),
+    ok.
+
