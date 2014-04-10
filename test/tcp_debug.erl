834ed45f6afa8b66e831215c3c7ca2c179cbc476
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2014-04-10 02:29:28 -0400

    cleanup tcp_debug api

--- test/tcp_debug.erl
+++ test/tcp_debug.erl
@@ -1,8 +1,12 @@
 -module(tcp_debug).
 
--compile(export_all).
-
 -define(TIMEOUT, 1000).
+
+%% Client API
+-export([connect/1, call/2, call/3, close/1]).
+
+%% Server API
+-export([remote_start/1, start/0, stop/1]).
 
 %% ===============
 %% Client 
@@ -10,7 +14,10 @@
 connect(Port) ->
     gen_tcp:connect("localhost", Port, [binary, {packet, 2}, {nodelay, true}]).
 
-call(Sock, {_M, _F, _A}=Data) ->
+call(Sock, Data) ->
+    call(Sock, Data, ?TIMEOUT).
+
+call(Sock, {_M, _F, _A}=Data, Timeout) ->
     inet:setopts(Sock, [{active, once}]),
     gen_tcp:send(Sock, term_to_binary(Data)),
     receive
@@ -20,6 +27,8 @@ call(Sock, {_M, _F, _A}=Data) ->
             E1;
         {tcp_error, Sock, _Reason}=E2 ->
             E2
+    after Timeout ->
+        {error, timeout}
     end.
 
 close(Sock) ->
@@ -32,8 +41,8 @@ close(Sock) ->
 remote_start(Node) ->
     rpc:call(Node, ?MODULE, start, [], ?TIMEOUT).
 
-start_link() ->
-    Pid = spawn_link(fun() ->
+start() ->
+    Pid = spawn(fun() ->
         {ok, ListenSock} = gen_tcp:listen(0, [binary, {active, false},
             {linger, {false, 0}}, {packet, 2}, {nodelay, true}]),
         {ok, Port} = inet:port(ListenSock),
@@ -47,13 +56,15 @@ start_link() ->
     receive
         Port -> Port
     end,
-    {ok, Port}.
+    {ok, Port, Pid}.
+
+%% Kill the server and all it's connections
+stop(Pid) ->
+     exit(Pid, kill).
     
 loop(ListenSock) ->
     {ok, Sock} = gen_tcp:accept(ListenSock),
-    Pid = spawn_link(fun() ->
-        debug_session(Sock)
-    end),
+    Pid = spawn_link(fun() -> debug_session(Sock) end),
     ok = gen_tcp:controlling_process(Sock, Pid),
     Pid ! ready,
     loop(ListenSock).
