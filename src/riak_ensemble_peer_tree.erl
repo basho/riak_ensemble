bc662baaaff1d38c1591f03d9ff5541675118acd
Martin Sumner <martin.sumner@adaptip.co.uk>
2020-04-21 13:58:01 +0100

    Remove gen_fsm_compat
    Make warnings errors again after removal.
    
    Lager is uplifted for consistency with other repos in the release

--- src/riak_ensemble_peer_tree.erl
+++ src/riak_ensemble_peer_tree.erl
@@ -20,6 +20,9 @@
 -module(riak_ensemble_peer_tree).
 -behaviour(gen_server).
 
+-compile({nowarn_deprecated_function, 
+            [{gen_fsm, send_event, 2}]}).
+
 %% API
 -export([start_link/3]).
 -export([get/2,
@@ -210,7 +213,7 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Hardcoded to send FSM event as expected by riak_ensemble_peer
 async_reply(From, Reply) when is_pid(From) ->
-    gen_fsm_compat:send_event(From, Reply).
+    gen_fsm:send_event(From, Reply).
 
 -spec do_get(_,state()) -> {any(), state()}.
 do_get(Key, State=#state{tree=Tree}) ->
